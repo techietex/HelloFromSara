03d1b66b34907ad94ff0ec4b80aa0cd1d8a04d1d
techietex <sarammack@byui.edu>
2021-10-07 19:16:40 -0500

    Initial commit

---
+++ src/Mack/Main.java
@@ -0,0 +1,10 @@
+package Mack;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello my name is Sara.");
+    }
+}
