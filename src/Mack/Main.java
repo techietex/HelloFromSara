dcb9fb0d153160bde4adaa2eaf14dbd98d7cb91d
techietex <sarammack@byui.edu>
2021-10-14 21:17:28 -0500

    Stretch goal

--- src/Mack/Main.java
+++ src/Mack/Main.java
@@ -8,5 +8,6 @@ public class Main {
 
         System.out.println("Hello my name is Sara.");
         System.out.println("Hello Sara! This is Dixie.");
+        System.out.println("I'm going to delete this line for the stretch goal.");
     }
 }
